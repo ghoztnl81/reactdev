8e27aee93792257643967b1c4c2b5f74c1aa6f0b
Ferdy van den Berg <fer@ghozt.nl>
2023-01-27 09:17:27 +0100

    Added and configged React-Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Home from './pages/Home'
+import Admin from './pages/Admin'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
     </div>
+    
   );
 }
 
